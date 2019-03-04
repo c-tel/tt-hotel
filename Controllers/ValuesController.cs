b6a4d352ac7ffc9bd1598fec635ed8f074972d71
c-tel <stas.telizhenko@gmail.com>
2019-03-04 11:25:45 +0200

    test db conn

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace TTHotelManagement.Controllers
 {
@@ -10,10 +11,38 @@ namespace TTHotelManagement.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private static string сonnectionString = 
+            "Host=ec2-54-243-128-95.compute-1.amazonaws.com;" +
+            "Port=5432;" +
+            "Username=tyoigfycditakg;" +
+            "Password=1bd83bca8ab334c28d2b311b0a713543c66a3288d3327aad1f910dea63fd372cd;" +
+            "Database=dcvtt5pjns4r4v";
+        private NpgsqlConnection conn;
+
+        public ValuesController()
+        {
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = "ec2-54-243-128-95.compute-1.amazonaws.com",
+                Port = 5432,
+                Username = "tyoigfycditakg",
+                Password = "1bd83bca8ab334c28d2b311b0a713543c66a3288d3327aad1f910dea63fd372cd",
+                Database = "dcvtt5pjns4r4v",
+                SslMode = SslMode.Require
+            };
+            conn = new NpgsqlConnection(builder.ToString());
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            conn.Open();
+            using (var cmd = new NpgsqlCommand("Select * From public.\"Users\"", conn))
+            {
+                var reader = cmd.ExecuteReader();
+            }
+            conn.Dispose();
             return new string[] { "value0", "value1" };
         }
 
